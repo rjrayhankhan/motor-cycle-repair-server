8d382cb46d5698f35b4a324bb6d4731aab3494c8
rjrayhankhan <aburayhan6212@gmail.com>
2021-12-09 01:12:07 +0600

    done the work

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send("hello from db it's working");
+})
+
+app.listen(process.env.PORT || port);
