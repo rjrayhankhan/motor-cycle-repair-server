b8f1758a25eabf32ffe38d4a46c91fd1f4461e50
rjrayhankhan <aburayhan6212@gmail.com>
2021-12-09 01:40:22 +0600

    i love you

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(cors());
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send("hello from db it's working");
+    res.send('hello from db it is working')
 })
 
-app.listen(process.env.PORT || port);
+
+app.listen(process.env.PORT || port)
